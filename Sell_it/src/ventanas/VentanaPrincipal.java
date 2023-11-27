4889caf3386a5d72550a1c0505b977ad3af88254
telmo <telmo@LAPTOP-38R5QIV3>
2023-11-27 23:10:34 +0100

    No esta terminado todavia lo de los scrolls

--- Sell_it/src/ventanas/VentanaPrincipal.java
+++ Sell_it/src/ventanas/VentanaPrincipal.java
@@ -7,21 +7,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
+import clases.Evento;
 import clases.Usuario;
 import datos.DataSetUsuario;
+import es.deusto.prog3.cap06.EjemploMuchosPanelesConScroll.Dato;
+import es.deusto.prog3.cap06.EjemploMuchosPanelesConScroll.MiPanel;
+import es.deusto.prog3.utils.JLabelGrafico;
 
 public class VentanaPrincipal extends JFrame{
 
 		private JTable tablaEventos;
 		private DataSetUsuario dataSetUsuario;
 		private VentanaInicio vent;
-		private List<VentanaEvento> listaEventos;//telmo
-		private JPanel pnlCentro;//telmo
+		private List<VentanaEvento> listaEventos;
+		private JPanel pnlCentro;
 		
 		public VentanaPrincipal(){
 			
@@ -32,7 +37,7 @@ public class VentanaPrincipal extends JFrame{
 			JLabel lblBusca = new JLabel("¡Busca el evento que desees!");
 			JPanel pnlNorte = new JPanel();
 			JPanel pnlSur = new JPanel();
-			pnlCentro = new JPanel(new GridLayout(3,2));//Telmo
+			
 			JScrollPane scrollCentro = new JScrollPane();
 			pnlNorte.setLayout(new FlowLayout());
 			DefaultTableModel modelo = new DefaultTableModel();
@@ -46,13 +51,15 @@ public class VentanaPrincipal extends JFrame{
 			
 			this.add(pnlNorte, BorderLayout.NORTH);
 			this.add(pnlSur, BorderLayout.SOUTH);
-			this.add(pnlCentro,BorderLayout.CENTER);//Telmo
 			pnlSur.add(bVenta);
 			pnlSur.add(bPerfil);
 			pnlNorte.add(lblBusca);
 			pnlNorte.add(tfBuscador);
 			pnlNorte.add(bBuscar);
-			pnlCentro.add(scrollCentro);//telmo
+			pnlCentro.setLayout(new BoxLayout(pnlCentro,BoxLayout.Y_AXIS));
+			add( new JScrollPane( pnlCentro ) , BorderLayout.CENTER );
+			
+			
 			
 			//this.add(tbl_buscar);
 			
@@ -140,12 +147,55 @@ public class VentanaPrincipal extends JFrame{
 		}
 		*/
 		
-		
-		private void agregarEventoAPanel(VentanaEvento evento) {//Telmo
-	        pnlCentro.add(evento.getNombreEvento());
+		private int numCol;  
+		private final int numColFila = 3;  
+		private JPanel pnlActual = null;   
+		public void empezarPanel() {
+			numCol = 4; 
+			pnlCentro.removeAll(); 
 		}
+		
+		public void aniadirEvento( Evento evento ) {
+			numCol++;  // Incremento la columna en la que se añade
+			if (numCol==numColFila) {  // Si está llena la fila (última columna) se empieza fila nueva
+				numCol = 0;
+				pnlActual = new JPanel();
+				pnlActual.setLayout(new BoxLayout(pnlActual,BoxLayout.X_AXIS));
+				pnlCentro.add( pnlActual );
+			}
+		//	pnlActual.add( new panel( /*titulo evento*/, /*Descripcion evento*/ ) ); 
+		}
+		public void acabarPanel() {
+			pnlCentro.revalidate(); 
+		}
+		
+		private static String[] fotos = new String[] { /*fotos*/ };
+		
+		//private static class panel extends JPanel {
+		//	public panel( String titulo, String descripcion ) {
+		//		setLayout( new BorderLayout() );
+		//		JLabel lblTitulo = new JLabel( titulo, JLabel.CENTER );
+		//		add( lblTitulo, BorderLayout.NORTH );
+		//		JTextArea taDescripcion = new JTextArea( descripcion, 2, 3);  // Atención - este marca el tamaño visual que va a "pedir" la textarea, 5 filas y 10 columnas
+		//		add( new JScrollPane( taDescripcion ), BorderLayout.CENTER );
+				//Aqui hay que decidir que fotos poner para cada evento
+			//	JLabelGrafico fotos = new JLabelGrafico( fotos, 50, 80 ); copio la case JLabelGrafico de Andoni??
+			//	add( fotos, BosrderLayout.EAST );
+		//	}
+		//}
+
+
+		
+		
+		
 		
 		public static void main(String[] args) {
 			VentanaPrincipal v = new VentanaPrincipal();
+		//	v.empezarPanel();
+		//	for (int i=0; i<7; i++) {
+		//		Evento evento = new Evento( "Evento " + /*Titulo del evento*/, "Decripcion " + /*Descripcion del Evento*/  );
+		//		v.aniadirEvento( evento );
+		//	}
+		//	v.acabarPanel();
 		}
 	}
