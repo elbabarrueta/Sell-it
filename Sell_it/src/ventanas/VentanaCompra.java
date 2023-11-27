237541de23449e5edcbd2bfffa87eeb2b1f61719
34619 <34619@DESKTOP-HFS29BT>
2023-11-27 23:45:50 +0100

    Timer compra

--- Sell_it/src/ventanas/VentanaCompra.java
+++ Sell_it/src/ventanas/VentanaCompra.java
@@ -1,22 +1,36 @@
 package ventanas;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.text.DecimalFormat;
+
 import javax.swing.*;
+
+import clases.Usuario;
 
 public class VentanaCompra extends JFrame{
 	
-	//Componentes
-	private JTextField tfNombre = new JTextField();
-	private JTextField tfApellidos = new JTextField();
-	private JTextField tfeMail = new JTextField();
-	private JTextField tfTfno = new JTextField();
-	private JTextField tfNtarjeta = new JTextField();
-	private JTextField tfMes = new JTextField("Mes");
-	private JTextField tfAnyo = new JTextField("Año");
+	private Usuario usuario;
+	
+	//Componentes de la ventana
+	private JTextField tfNombre;
+	private JTextField tfCorreo;
+	private JTextField tfTfno;
+	private JTextField tfNtarjeta;
+	private JComboBox cbMes;
+	private JComboBox cbAnyo;
+	private JLabel lTiempo;
 
+	private Timer timer;
+    private int tiempoRestante;
 
 	
-	public VentanaCompra() {
+	public VentanaCompra(Usuario usuario) {
+		this.usuario = usuario;
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setSize(600, 500);
 		setLocationRelativeTo(null);
@@ -25,69 +39,110 @@ public class VentanaCompra extends JFrame{
 		//Creamos los paneles principales
 		JPanel pCentral = new JPanel(new GridLayout(2,1));
 		this.add(pCentral, BorderLayout.CENTER);
-		JPanel pIzq = new JPanel(new BorderLayout());
-		this.add(pIzq, BorderLayout.EAST);
+		JPanel pDer = new JPanel(new BorderLayout());
+		this.add(pDer, BorderLayout.EAST);
 		
 		
 		//Panel Central
 		//Dentro del panel central creamos 2 paneles
-		JPanel pDatos = new JPanel(new GridLayout(5,2));
+		JPanel pDatos = new JPanel(new GridLayout(4,1));
 		pCentral.add(pDatos);
-		JPanel pPago = new JPanel(new GridLayout(3,2));
+		JPanel pPago = new JPanel(new GridLayout(3,1));
 		pCentral.add(pPago);
 		
 		
 		//Panel Datos
+		JPanel pTitulo = new JPanel();
 		JLabel lDatos = new JLabel("Tus Datos");
-		JLabel lUsuario = new JLabel("Nombre Ususario");
-		pDatos.add(lDatos);
-		pDatos.add(lUsuario);
+		pTitulo.add(lDatos, BorderLayout.CENTER);
+		pDatos.add(pTitulo);
 		
+		JPanel pNombre = new JPanel();
 		JLabel lNombre = new JLabel("Nombre");
-		pDatos.add(lNombre);
-		pDatos.add(tfNombre);
-
-		JLabel lApellidos = new JLabel("Apellidos");
-		pDatos.add(lApellidos);
-		pDatos.add(tfApellidos);
-
-		JLabel leMail = new JLabel("eMail");
-		pDatos.add(leMail);
-		pDatos.add(tfeMail);
-
+		tfNombre = new JTextField();
+		tfNombre.setText(usuario.getNombreUsuario());
+		tfNombre.setPreferredSize(new Dimension(200, 20));
+		pNombre.add(lNombre);
+		pNombre.add(tfNombre);
+		pDatos.add(pNombre);
+		
+		JPanel pCorreo = new JPanel();
+		JLabel lCorreo = new JLabel("Correo");
+		tfCorreo = new JTextField();
+		tfCorreo.setText(usuario.getCorreoUsuario());
+		tfCorreo.setPreferredSize(new Dimension(200, 20));
+		pCorreo.add(lCorreo);
+		pCorreo.add(tfCorreo);
+		pDatos.add(pCorreo);
+		
+		JPanel pTfno = new JPanel();
 		JLabel lTfno = new JLabel("Teléfono");
-		pDatos.add(lTfno);
-		pDatos.add(tfTfno);
+		tfTfno = new JTextField();
+		tfTfno.setPreferredSize(new Dimension(200, 20));
+		pTfno.add(lTfno);
+		pTfno.add(tfTfno);
+		pDatos.add(pTfno);
 		
 		
 		//Panel Método de Pago
+		JPanel pMetodoPago = new JPanel();
 		JLabel lPago = new JLabel("Método de pago");
-		pPago.add(lPago);
-		JLabel l = new JLabel("");
-		pPago.add(l);
+		pMetodoPago.add(lPago);
+		pPago.add(pMetodoPago);
+		
+		JPanel pNTarjeta = new JPanel();
 		JLabel lNtarjeta = new JLabel("Nº tarjeta");
-		pPago.add(lNtarjeta);
-		pPago.add(tfNtarjeta);
+		tfNtarjeta = new JTextField();
+		tfNtarjeta.setPreferredSize(new Dimension(200, 20));
+		pNTarjeta.add(lNtarjeta);
+		pNTarjeta.add(tfNtarjeta);
+		pPago.add(pNTarjeta);
+		
+		JPanel pFCad = new JPanel();
 		JLabel lFechaCad = new JLabel("Fecha de caducidad");
-		pPago.add(lFechaCad);
+		pFCad.add(lFechaCad);
 		
 		JPanel pFC = new JPanel(new GridLayout(1,1));
-		pFC.add(tfMes);
-		pFC.add(tfAnyo);
-		pPago.add(pFC);
+		String[] meses = {"MES", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"};
+		cbMes = new JComboBox<>(meses);
+		String[] anyos = {"AÑO", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030", "2031"};
+		cbAnyo = new JComboBox<>(anyos);
+
+ 		pFC.add(cbMes);
+		pFC.add(cbAnyo);
+		pFCad.add(pFC);
+		
+		pPago.add(pFCad);
 		
 		
 		//Panel Lateral
 		//Creamos 2 paneles
+		
 		JPanel pTiempo = new JPanel(new GridLayout(3,1));
-		pIzq.add(pTiempo, BorderLayout.NORTH);
+		pDer.add(pTiempo, BorderLayout.NORTH);
 		JPanel pConfirmar = new JPanel();
-		pIzq.add(pConfirmar, BorderLayout.SOUTH);
+		pDer.add(pConfirmar, BorderLayout.SOUTH);
 		
 		
 		//Panel Tiempo restante y Total
-		JLabel lTiempo = new JLabel("Tiempo restante");
+		lTiempo = new JLabel("Tiempo restante: 10:00");
+		lTiempo.setHorizontalAlignment(SwingConstants.CENTER);
 		pTiempo.add(lTiempo);
+		
+		tiempoRestante = 600; //10 minutos en segundos
+		timer = new Timer(1000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				actualizarTiempo();
+			}
+		});
+		
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				timer.start();
+			}
+		});
 		
 		JLabel lTotal = new JLabel("TOTAL: €");
 		pTiempo.add(lTotal);
@@ -97,11 +152,42 @@ public class VentanaCompra extends JFrame{
 		JButton bConfirmar = new JButton("CONFIRMAR COMPRA");
 		pConfirmar.add(bConfirmar);
 		
-		
-		
+		bConfirmar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				confirmarCompra();
+			}
+		});
 	}
+
+	private void actualizarTiempo() {
+		tiempoRestante--;
+		if(tiempoRestante >= 0) {
+			int minutos = tiempoRestante/60;
+			int segundos = tiempoRestante%60;
+			
+			DecimalFormat formato = new DecimalFormat("00");
+			lTiempo.setText("Tiempo restante: " + formato.format(minutos) + ":" + formato.format(segundos));
+		}else {
+			timer.stop();
+			JOptionPane.showMessageDialog(null, "Límite de tiempo excedido. Por favor, inténtalo de nuevo", "Tiempo agotado", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	private void confirmarCompra() {
+		if(tfNombre.getText().equals("")|| tfCorreo.getText().equals("") || tfTfno.getText().equals("") || tfNtarjeta.getText().equals("") || cbMes.getSelectedIndex() == 0 || cbAnyo.getSelectedIndex() == 0) {
+            JOptionPane.showMessageDialog(null, "Para confirmar la comra debe introducir todos los datos.");
+            return;
+		}else {
+			JOptionPane.showMessageDialog(null, "¡Compra confirmada!", "Confirmación", JOptionPane.INFORMATION_MESSAGE);
+			dispose();
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		VentanaCompra v = new VentanaCompra();
+		Usuario u = new Usuario("Laura Lopez","laura.lopez@gmail.com","Usuario corriente","abcABC33");
+		VentanaCompra v = new VentanaCompra(u);
 		v.setVisible(true);
 	}
 
