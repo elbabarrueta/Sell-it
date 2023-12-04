78c10f4834e4e3c75c0615f3b6ed3cbcdf1452a6
diego <diego@192.168.1.136>
2023-12-04 17:27:43 +0100

    update ventanalogo

--- Sell_it/src/ventanas/VentanaLogoPrincipal.java
+++ Sell_it/src/ventanas/VentanaLogoPrincipal.java
@@ -1,47 +1,65 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class VentanaLogoPrincipal extends JFrame{
-	
-//	public static void main(String[] args) {
-//		crearVentanaSell_It();
-//	}
+public class VentanaLogoPrincipal extends JFrame {
 
-	private static void crearVentanaSell_It() {
-        JFrame ventana = new JFrame("Sell It");
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ImageIcon icono = new ImageIcon("Sell_it\\src\\imagenes\\sell_it.png");
+    private static final long serialVersionUID = 1L;
+
+    public VentanaLogoPrincipal() {
+        setTitle("Sell It");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon icono = new ImageIcon("Sell_it/src/imagenes/sell_it.png");
         JLabel imagen = new JLabel(icono);
-//        JPanel pnlImagen = new JPanel();
-//        JPanel pnlCarga = new JPanel();
-		//JLabel cargando = new JLabel("Cargando Sell It...");
-//		public void hilo{
-//        for(int i = 0; i<5; i++) {
-//		   cont.inc(1);
-//        	Thread.sleep(1000);
-//        	int s = i;
-//        }
-//		pnlImagen.add(imagen);
-//		pnlCarga.add(cargando);
-        ventana.getContentPane().add(imagen, BorderLayout.CENTER);
-      //  ventana.add(cargando, BorderLayout.SOUTH);
-        ventana.setSize(400, 300);
-        ventana.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        ventana.setVisible(true);
+        JLabel mensaje = new JLabel("Cargando Sell It...");
+        mensaje.setFont(new Font("Arial", Font.PLAIN, 20));
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(imagen, BorderLayout.CENTER);
         
+        JPanel panelSur = new JPanel(new GridBagLayout());
+        panelSur.add(mensaje);
+        
+        panel.add(panelSur, BorderLayout.SOUTH);
+
+        getContentPane().add(panel);
+
+        setSize(400, 300);
+        setLocationRelativeTo(null); // Centra la ventana en la pantalla
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setVisible(true);
+
+        // Inicia el hilo de cuenta regresiva
+        contarRegresivamente(mensaje);
     }
-	
-	private int valor;
-	
-	public void inc( int incremento ) {
-		int temporal = valor;
-		temporal = temporal + incremento;
-		valor = temporal;
-	}
+
+    private void contarRegresivamente(JLabel mensaje) {
+        AtomicInteger segundosRestantes = new AtomicInteger(5);
+
+        Thread hiloCuentaRegresiva = new Thread(() -> {
+            while (segundosRestantes.get() > 0) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                int segundos = segundosRestantes.decrementAndGet();
+                mensaje.setText("Cargando Sell It... " + segundos + " segundos");
+            }
+
+            mensaje.setText("Cargando Sell It... ¡Completo!");
+        });
+
+        hiloCuentaRegresiva.start();
+    }
 }
