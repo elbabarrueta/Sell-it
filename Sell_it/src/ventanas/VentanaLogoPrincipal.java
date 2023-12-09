da654a44749d3fe702a304ca8986066b6d92914a
diego <diego@ASUS-DIEGO>
2023-12-09 11:29:15 +0100

    update ventanalogo

--- Sell_it/src/ventanas/VentanaLogoPrincipal.java
+++ Sell_it/src/ventanas/VentanaLogoPrincipal.java
@@ -3,12 +3,14 @@ package ventanas;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
 public class VentanaLogoPrincipal extends JFrame {
 
@@ -23,12 +25,16 @@ public class VentanaLogoPrincipal extends JFrame {
         JLabel mensaje = new JLabel("Cargando Sell It...");
         mensaje.setFont(new Font("Arial", Font.PLAIN, 20));
 
+        JProgressBar progressBar = new JProgressBar(0, 100);
+        progressBar.setStringPainted(true);
+
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(imagen, BorderLayout.CENTER);
-        
+
         JPanel panelSur = new JPanel(new GridBagLayout());
         panelSur.add(mensaje);
-        
+        panelSur.add(progressBar);
+
         panel.add(panelSur, BorderLayout.SOUTH);
 
         getContentPane().add(panel);
@@ -38,14 +44,16 @@ public class VentanaLogoPrincipal extends JFrame {
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setVisible(true);
 
-        // Inicia el hilo de cuenta regresiva
-        contarRegresivamente(mensaje);
+        // Inicia el hilo de cuenta regresiva y progreso aleatorio
+        contarRegresivamente(mensaje, progressBar);
     }
 
-    private void contarRegresivamente(JLabel mensaje) {
-        AtomicInteger segundosRestantes = new AtomicInteger(5);
+    private void contarRegresivamente(JLabel mensaje, JProgressBar progressBar) {
+        AtomicInteger segundosRestantes = new AtomicInteger(4);
 
         Thread hiloCuentaRegresiva = new Thread(() -> {
+            Random random = new Random();
+
             while (segundosRestantes.get() > 0) {
                 try {
                     Thread.sleep(1000);
@@ -55,11 +63,22 @@ public class VentanaLogoPrincipal extends JFrame {
 
                 int segundos = segundosRestantes.decrementAndGet();
                 mensaje.setText("Cargando Sell It... " + segundos + " segundos");
+
+                // Simula un progreso aleatorio en la barra de progreso
+                int progreso = 100 - (segundos * 25); // Incrementa progresivamente cada segundo
+                progreso += random.nextInt(11); // Agrega un valor aleatorio entre 0 y 10
+
+                progressBar.setValue(progreso);
             }
 
             mensaje.setText("Cargando Sell It... ¡Disfruta de tu estancia!");
+            progressBar.setValue(100); // Asegura que la barra de progreso alcance el 100% al final
         });
 
         hiloCuentaRegresiva.start();
+    }
+
+    public static void main(String[] args) {
+        new VentanaLogoPrincipal();
     }
 }
