bd44c0e29474d6e5bd1636ca263b4f70c31933d2
diego <diego@ASUS-DIEGO>
2023-12-26 14:02:00 +0100

    UPD

--- Sell_it/src/ventanas/VentanaInicio.java
+++ Sell_it/src/ventanas/VentanaInicio.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
@@ -194,7 +195,12 @@ public class VentanaInicio extends JFrame {
 			}
 		});
 		
-		BaseDeDatos.main(null);
+		try {
+			BaseDeDatos.main(null);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		base = new BaseDeDatos();
 		base.verUsuarios();
 		base.verEvento();
